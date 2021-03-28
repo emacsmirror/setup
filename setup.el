439b4a89c76c01d0127ee1cdcad9f9493804a525
Philip K <philipk@posteo.net>
2021-03-28 15:33:38 +0200

    Raise an error if lexical binding is not enabled

--- setup.el
+++ setup.el
@@ -106,6 +106,8 @@ will otherwise just be evaluated as is.
 NAME may also be a macro, if it can provide a symbol."
   (declare (debug (&rest &or [symbolp sexp] form))
            (indent defun))
+  (unless lexical-binding
+    (error "The `setup' macro requires lexical binding"))
   (when (consp name)
     (push name body)
     (let ((shorthand (get (car name) 'setup-shorthand)))
